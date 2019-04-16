e5071c77b7c23723afc4dc7c6b5334ca9451cb77
TobiasDing <tobias.ding@hotmail.com>
2019-04-16 12:34:55 +0800

    download

--- download.py
+++ download.py
@@ -1,10 +1,12 @@
 import os
 
-data = ['www.baidu.com']
+data = ['http://jsoc.stanford.edu/SUM72/D1154071488/S00000/JSOC_20190412_078.tar']
 
 for item in data:
     # os.popen('cd /Users/dingweiqi/Documents/datas')
     # os.chdir("/Users/dingweiqi/Documents/datas");
-    tmpres = os.popen('curl -o /Users/dingweiqi/Documents/datas/file.txt %s' % item).readlines()
+    # a = os.popen('export all_proxy="socks5://127.0.0.1:1086"').readlines()
+    # print(a)
+    tmpres = os.popen('export all_proxy="socks5://127.0.0.1:1086" ; curl -o /Users/dingweiqi/Documents/datas/file.tar %s' % item).readlines()
     print(tmpres)
-
+    print(111)
